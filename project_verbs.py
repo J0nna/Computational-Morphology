42fce9adb94737b953e11757fdcf6ba3165123cb
J0nna <58913030+J0nna@users.noreply.github.com>
2022-02-12 17:15:42 +0200

    Saving the project to git

---
+++ project_verbs.py
@@ -0,0 +1,59 @@
+verb=input("Give a verb\n")
+
+print()
+
+print("down "+verb+"+Inf")
+print("down "+verb+"+Pre+St2ps")
+print("down "+verb+"+Pre+Cv2ps")
+
+print("down "+verb+"+Pret+St2ps")
+print("down "+verb+"+Pret+Cv2ps")
+
+print("down "+verb+"+Imp+St2ps")
+print("down "+verb+"+Imp+Cv2ps")
+print("down "+verb+"+Cond+St2ps")
+print("down "+verb+"+Cond+Cv2ps")
+print("down "+verb+"+Fut+St2ps")
+print("down "+verb+"+Fut+Cv2ps")
+print("down "+verb+"+SubjPre+St2ps")
+print("down "+verb+"+SubjPre+Cv2ps")
+print("down "+verb+"+SubjImp+St2ps")
+print("down "+verb+"+SubjImp+Cv2ps")
+
+print("down "+verb+"+SubjFut+St2ps")
+print("down "+verb+"+SubjFut+Cv2ps")
+
+print("down "+verb+"+PerPre+St2ps")
+print("down "+verb+"+PerPre+Cv2ps")
+print("down "+verb+"+PerPret+St2ps")
+print("down "+verb+"+PerPret+Cv2ps")
+print("down "+verb+"+PerPast+St2ps")
+print("down "+verb+"+PerPast+Cv2ps")
+
+print("down "+verb+"+PerCond+St2ps")
+print("down "+verb+"+PerCond+Cv2ps")
+print("down "+verb+"+PerFut+St2ps")
+print("down "+verb+"+PerFut+Cv2ps")
+print("down "+verb+"+PerSubjPre+St2ps")
+print("down "+verb+"+PerSubjPre+Cv2ps")
+
+print("down "+verb+"+PerSubjPast+St2ps")
+print("down "+verb+"+PerSubjPast+Cv2ps")
+print("down "+verb+"+PerSubjFut+St2ps")
+print("down "+verb+"+PerSubjFut+Cv2ps")
+
+print("down "+verb+"+ConPre+St2ps")
+print("down "+verb+"+ConPre+Cv2ps")
+print("down "+verb+"+ConPret+St2ps")
+print("down "+verb+"+ConPret+Cv2ps")
+print("down "+verb+"+ConImp+St2ps")
+print("down "+verb+"+ConImp+Cv2ps")
+print("down "+verb+"+ConCond+St2ps")
+print("down "+verb+"+ConCond+Cv2ps")
+
+print("down "+verb+"+Impera+St2ps")
+print("down "+verb+"+Impera+Cv2ps")
+print("down "+verb+"+ImperaNeg+St2ps")
+print("down "+verb+"+ImperaNeg+Cv2ps")
+
+print()
